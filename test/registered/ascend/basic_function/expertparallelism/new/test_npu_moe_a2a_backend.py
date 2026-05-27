1f104d9ca343d73da389f29bec1bc3f97c907c3b
YZY00Raiser <2452658175@qq.com>
2026-05-27 17:16:27 +0800

    update

---
+++ test/registered/ascend/basic_function/expertparallelism/new/test_npu_moe_a2a_backend.py
@@ -0,0 +1,97 @@
+import unittest
+
+import requests
+
+from sglang.srt.utils import kill_process_tree
+# from sglang.test.ascend.test_ascend_utils import DEEPSEEK_V3_2_W8A8_WEIGHTS_PATH
+from sglang.test.ci.ci_register import register_npu_ci
+from sglang.test.test_utils import (
+    DEFAULT_TIMEOUT_FOR_SERVER_LAUNCH,
+    DEFAULT_URL_FOR_TEST,
+    CustomTestCase,
+    popen_launch_server,
+)
+
+register_npu_ci(est_time=400, suite="nightly-2-npu-a3", nightly=True)
+DEEPSEEK_V3_2_W8A8_WEIGHTS_PATH = "/home/weights/DeepSeek-V3.2-W8A8"
+
+
+class TestMoreRunnerBackendTriton(CustomTestCase):
+    """Testcase：Verify set --moe-a2a-backend, the inference request is successfully processed.
+
+    [Test Category] Parameter
+    [Test Target] --moe-a2a-backend
+    """
+
+    model = DEEPSEEK_V3_2_W8A8_WEIGHTS_PATH
+    moe_runner_backend = "triton"
+
+    @classmethod
+    def setUpClass(cls):
+        cls.process = popen_launch_server(
+            cls.model,
+            DEFAULT_URL_FOR_TEST,
+            DEFAULT_TIMEOUT_FOR_SERVER_LAUNCH,
+            other_args=[
+                "--trust-remote-code",
+                "--attention-backend",
+                "ascend",
+                "--disable-cuda-graph",
+                "--mem-fraction-static",
+                "0.5",
+                "--tp-size",
+                "2",
+                "--ep",
+                "2",
+                # "--enable-eplb",
+                "--moe-a2a-backend",
+                "ascend_fuseep",  # It is incompatible with eplb
+                # "--moe-a2a-backend",
+                # "deepep",
+                # "--deepep-mode",
+                # "normal",
+                # "--ep-num-redundant-experts",
+                # "4",
+                # "--expert-distribution-recorder-buffer-size",
+                # "50",
+                # "--moe-runner-backend",
+                # cls.moe_runner_backend,
+                "--base-gpu-id",
+                "2"
+            ],
+            env={
+                "SGLANG_NPUDISABLE_ACL_FORMAT_WEIGHT": "1",
+                "HCCL_BUFFSIZE": "1024",
+                "SGLANG_NPU_FUSED_MOE_MODE": "1",
+            },
+        )
+
+    @classmethod
+    def tearDownClass(cls):
+        kill_process_tree(cls.process.pid)
+
+    def test_moe_runner_backend(self):
+        response = requests.post(
+            f"{DEFAULT_URL_FOR_TEST}/generate",
+            json={
+                "text": "The capital of France is",
+                "sampling_params": {
+                    "temperature": 0,
+                    "max_new_tokens": 32,
+                },
+            },
+        )
+        self.assertEqual(
+            response.status_code, 200, "The request status code is not 200."
+        )
+        self.assertIn(
+            "Paris", response.text, "The inference result does not include Paris."
+        )
+
+
+# class TestMoreRunnerBackendTritonDefault(TestMoreRunnerBackendTriton):
+#     moe_runner_backend = "auto"
+
+
+if __name__ == "__main__":
+    unittest.main()
