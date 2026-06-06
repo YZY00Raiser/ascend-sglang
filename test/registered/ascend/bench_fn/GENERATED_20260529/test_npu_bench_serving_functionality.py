eb705a2d3569c52fdcfa1ee7b5846617f099cda8
YZY00Raiser <YZY00Raiser@example.com>
2026-06-06 14:33:18 +0800

    Fix: add missing gsp_group_distribution attribute

--- test/registered/ascend/bench_fn/GENERATED_20260529/test_npu_bench_serving_functionality.py
+++ test/registered/ascend/bench_fn/GENERATED_20260529/test_npu_bench_serving_functionality.py
@@ -71,6 +71,7 @@ class TestNpuBenchServingFunctionality(CustomTestCase):
                     gsp_num_turns=NUM_TURNS,
                 )
                 args.warmup_requests = 0
+                args.gsp_group_distribution = "uniform"
                 res = run_benchmark(args)
                 self.assertEqual(res["completed"], NUM_CONVERSATIONS * NUM_TURNS)
 
