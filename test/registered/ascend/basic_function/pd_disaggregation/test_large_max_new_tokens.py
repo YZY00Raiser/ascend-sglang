f7eeb5815fe83d17c303b3d9f5b9d613f4b70200
YZY00Raiser <2452658175@qq.com>
2026-06-10 17:09:39 +0800

    update

--- test/registered/ascend/basic_function/pd_disaggregation/test_large_max_new_tokens.py
+++ test/registered/ascend/basic_function/pd_disaggregation/test_large_max_new_tokens.py
@@ -42,7 +42,7 @@ class TestLargeMaxNewTokens(CustomTestCase):
             api_key=cls.api_key,
             other_args=(
                 "--max-total-token",
-                "1536",
+                "1550",
                 "--context-len",
                 "8192",
                 "--decode-log-interval",
