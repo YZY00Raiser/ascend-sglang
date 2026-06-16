566cb92ba5920846919c3012b08732ceea830176
YZY00Raiser <2452658175@qq.com>
2026-06-16 21:52:31 +0800

    update

--- test/registered/ascend/basic_function/612/model.sh
+++ test/registered/ascend/basic_function/612/model.sh
@@ -1,7 +1,7 @@
 from sglang.test.ascend.test_ascend_utils import GTE_QWEN2_1_5B_INSTRUCT_WEIGHTS_PATH
 from sglang.test.ascend.test_ascend_utils import LLAMA_3_2_1B_INSTRUCT_WEIGHTS_PATH
 from sglang.test.ascend.test_ascend_utils import QWEN3_0_6B_WEIGHTS_PATH
-
+from sglang.test.ascend.test_ascend_utils import DEEPSEEK_V2_LITE_W8A8_WEIGHTS_PATH
 
 QWEN3_0_6B_WEIGHTS_PATH="/home/weights/Qwen/Qwen3-0.6B"
 
