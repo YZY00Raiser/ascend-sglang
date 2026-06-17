870fb7f9050db62ae75af3029751771b1c33aaf4
YZY00Raiser <2452658175@qq.com>
2026-06-17 16:27:46 +0800

    update

--- test/registered/ascend/basic_function/617/test_npu_anthropic_tool_use.py
+++ test/registered/ascend/basic_function/617/test_npu_anthropic_tool_use.py
@@ -7,7 +7,8 @@ import unittest
 import requests
 
 from sglang.srt.utils import kill_process_tree
-from sglang.test.ci.ci_register import register_amd_ci, register_cuda_ci
+from sglang.test.ci.ci_register import register_npu_ci
+from sglang.test.ascend.test_ascend_utils import QWEN3_0_6B_WEIGHTS_PATH
 from sglang.test.test_utils import (
     DEFAULT_SMALL_MODEL_NAME_FOR_TEST,
     DEFAULT_TIMEOUT_FOR_SERVER_LAUNCH,
@@ -16,8 +17,7 @@ from sglang.test.test_utils import (
     popen_launch_server,
 )
 
-register_cuda_ci(est_time=50, stage="base-b", runner_config="1-gpu-large")
-register_amd_ci(est_time=140, suite="stage-b-test-1-gpu-small-amd")
+register_npu_ci(est_time=400, suite="full-1-npu-a3", nightly=True)
 
 # System message to guide Llama3.2 to produce proper tool call format
 SYSTEM_MESSAGE = (
@@ -78,7 +78,9 @@ class TestAnthropicToolUse(CustomTestCase):
             api_key=cls.api_key,
             other_args=[
                 "--tool-call-parser",
-                "llama3",
+                "qwen",
+                "--attention-backend",
+                "ascend",
             ],
         )
         cls.messages_url = cls.base_url + "/v1/messages"
