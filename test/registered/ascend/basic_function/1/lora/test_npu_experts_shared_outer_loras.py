88fb9555c349927dcd25f0fcac4861591da3c021
YZY00Raiser <2452658175@qq.com>
2026-07-04 00:41:50 +0800

    update

--- test/registered/ascend/basic_function/1/lora/test_npu_experts_shared_outer_loras.py
+++ test/registered/ascend/basic_function/1/lora/test_npu_experts_shared_outer_loras.py
@@ -129,7 +129,7 @@ class TestLora2(TestLora1):
             "--disable-cuda-graph",
             "--experts-shared-outer-loras"
             "--lora-drain-wait-threshold"
-            "0.0"
+            "0"
         ]
         cls.process = popen_launch_server(
             LLAMA_3_2_1B_INSTRUCT_WEIGHTS_PATH,
