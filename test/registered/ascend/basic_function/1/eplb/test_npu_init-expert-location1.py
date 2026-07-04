5601f491195625565a6bffc19f6048c3b5ed7362
YZY00Raiser <2452658175@qq.com>
2026-07-04 16:57:39 +0800

    update

---
+++ test/registered/ascend/basic_function/1/eplb/test_npu_init-expert-location1.py
@@ -0,0 +1,58 @@
+import unittest
+import json
+
+from sglang.test.ascend.gsm8k_ascend_mixin import GSM8KAscendMixin
+from sglang.test.ci.ci_register import register_npu_ci
+from sglang.test.test_utils import CustomTestCase
+
+QWEN3_30B_A3B_INSTRUCT_2507_WEIGHTS_PATH = "/home/weights/Qwen3-30B-A3B"
+
+# 读取专家分布文件内容，作为参数值
+with open("/home/d30060301/pt/qwen3_30b_a3b_random_no_redundant.json", "r") as f:
+    init_expert_location = f.read()
+
+class TestQwen330B(GSM8KAscendMixin, CustomTestCase):
+    """Testcase: Verify that the inference accuracy of the /home/weights/Qwen3-30B-A3B model on the GSM8K dataset is no less than 0.90.
+
+    [Test Category] Model
+    [Test Target] /home/weights/Qwen3-30B-A3B
+    """
+
+    model = QWEN3_30B_A3B_INSTRUCT_2507_WEIGHTS_PATH
+    accuracy = 0.90
+    other_args = [
+        #"--enable-metrics",
+        #"--enable-expert-distribution-metrics",
+        "--trust-remote-code",
+        "--mem-fraction-static",
+        0.7,
+        "--expert-distribution-recorder-mode",
+        "stat",
+        "--max-running-requests",
+        32,
+        "--attention-backend",
+        "ascend",
+        "--disable-cuda-graph",
+        "--cuda-graph-max-bs",
+        32,
+        "--tp-size",
+        2,
+        "--ep-dispatch-algorithm",
+        "static",
+        "--moe-a2a-backend",
+        "deepep",
+        "--deepep-mode",
+        "normal",
+        "--init-expert-location",
+        init_expert_location   # 直接使用文件内容，而非文件路径
+    ]
+
+    env = {
+        "HCCL_BUFFSIZE": "1024",
+        #"SGLANG_EPLB_HEATMAP_COLLECTION_INTERVAL": "100",
+        "SGLANG_LOG_EXPERT_LOCATION_METADATA": "1"  # 可以从打屏看到更多信息，用不到的话可以删掉
+    }
+
+
+if __name__ == "__main__":
+    unittest.main()
