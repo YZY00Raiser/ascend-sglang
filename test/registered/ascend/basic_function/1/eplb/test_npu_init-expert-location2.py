58ed83af70151cadf36d2433b7ffafb359849b53
YZY00Raiser <2452658175@qq.com>
2026-07-06 09:38:26 +0800

    update

--- test/registered/ascend/basic_function/1/eplb/test_npu_init-expert-location2.py
+++ test/registered/ascend/basic_function/1/eplb/test_npu_init-expert-location2.py
@@ -37,9 +37,9 @@ class TestQwen330B(GSM8KAscendMixin, CustomTestCase):
         "normal",
         "--init-expert-location",
         # "expert_distribution_recorder_1783072007.920626.pt",
-        #"/home/d30060301/pt/expert_distribution_recorder.pt"
+        "expert_distribution_recorder.pt"
         # "expert_distribution_recorder.json",
-        "qwen3_30b_a3b_random_no_redundant.json",
+        # "qwen3_30b_a3b_random_no_redundant.json",
     ]
 
     env = {
