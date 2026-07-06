c5c106a73c5ef9ecfa83455647f3c93b6619ef5d
YZY00Raiser <2452658175@qq.com>
2026-07-06 11:15:44 +0800

    update

---
+++ test/registered/ascend/basic_function/1/lora/test_npu_experts_shared_outer_lora.py
@@ -0,0 +1,89 @@
+import tempfile
+import unittest
+from types import SimpleNamespace
+
+from sglang.srt.utils import kill_process_tree
+from sglang.test.ci.ci_register import register_npu_ci
+from sglang.test.run_eval import run_eval
+from sglang.test.test_utils import (
+    DEFAULT_TIMEOUT_FOR_SERVER_LAUNCH,
+    DEFAULT_URL_FOR_TEST,
+    CustomTestCase,
+    popen_launch_server,
+)
+from sglang.test.ascend.test_ascend_utils import (
+    QWEN3_30B_A3B_INSTRUCT_2507_WEIGHTS_PATH,
+)
+LORA_HF_REPO = "yushengsu/lora-diff-Qwen3-30B-A3B-Instruct-2507"
+register_npu_ci(est_time=200, suite="full-16-npu-a3", nightly=True)
+
+
+class TestDeepSeekV32(CustomTestCase):
+    """Testcase: Verify that the inference accuracy of the vllm-ascend/DeepSeek-V3.2-W8A8 model on the GSM8K dataset is no less than 0.95.
+
+    [Test Category] Model
+    [Test Target] vllm-ascend/DeepSeek-V3.2-W8A8
+    """
+
+    @classmethod
+    def setUpClass(cls):
+        cls.model = QWEN3_30B_A3B_INSTRUCT_2507_WEIGHTS_PATH
+        cls.base_url = DEFAULT_URL_FOR_TEST
+        cls.out_log_file = tempfile.NamedTemporaryFile(
+            mode="w+", delete=True, suffix="out.log"
+        )
+        cls.err_log_file = tempfile.NamedTemporaryFile(
+            mode="w+", delete=True, suffix="err.log"
+        )
+        cls.process = popen_launch_server(
+            cls.model,
+            cls.base_url,
+            timeout=DEFAULT_TIMEOUT_FOR_SERVER_LAUNCH,
+            other_args=[
+                "--trust-remote-code",
+                "--mem-fraction-static",
+                "0.9",
+                "--attention-backend",
+                "ascend",
+                "--disable-cuda-graph",
+                "--tp-size",
+                "16",
+                "--quantization",
+                "modelslim",
+                "--disable-radix-cache",
+                "--enable-deepep-waterfill"
+            ],
+            return_stdout_stderr=(cls.out_log_file, cls.err_log_file),
+            env={
+                "HCCL_BUFFSIZE": "2048",
+            },
+        )
+
+    @classmethod
+    def tearDownClass(cls):
+        kill_process_tree(cls.process.pid)
+        cls.out_log_file.close()
+        cls.err_log_file.close()
+
+    def test_gsm8k(self):
+        args = SimpleNamespace(
+            base_url=self.base_url,
+            model=self.model,
+            eval_name="gsm8k",
+            api="completion",
+            max_tokens=512,
+            num_examples=128,
+            num_threads=200,
+        )
+        metrics = run_eval(args)
+        print(f"Eval accuracy of GSM8K: {metrics=}")
+
+        self.assertGreater(metrics["score"], 0.95)
+        self.err_log_file.seek(0)
+        content = self.err_log_file.read()
+        error_message = "DeepEP Waterfill is enabled"
+        self.assertIn(error_message, content)
+
+
+if __name__ == "__main__":
+    unittest.main()
