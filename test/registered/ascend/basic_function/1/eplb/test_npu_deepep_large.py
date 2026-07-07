53a8f4d79e887140da066b00564ea18f7fabea0c
YZY00Raiser <2452658175@qq.com>
2026-07-07 16:23:08 +0800

    update

--- test/registered/ascend/basic_function/1/eplb/test_npu_deepep_large.py
+++ test/registered/ascend/basic_function/1/eplb/test_npu_deepep_large.py
@@ -55,7 +55,7 @@ class TestDeepseek(CustomTestCase):
                 "--model-loader-extra-config",
                 '{"enable_multithread_load": true,"num_threads": 64}',
                 "--mem-fraction-static",
-                "0.9",
+                "0.75",
             ],
         )
 
@@ -114,7 +114,7 @@ class TestDeepseek2(TestDeepseek):
                 "--model-loader-extra-config",
                 '{"enable_multithread_load": true,"num_threads": 64}',
                 "--mem-fraction-static",
-                "0.9",
+                "0.75",
             ],
         )
 
@@ -163,7 +163,7 @@ class TestDeepseekMTP(CustomTestCase):
                 "--model-loader-extra-config",
                 '{"enable_multithread_load": true,"num_threads": 64}',
                 "--mem-fraction-static",
-                "0.9",
+                "0.75",
             ],
         )
 
