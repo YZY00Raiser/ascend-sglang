0a7426700471d3e67ebd7b601558787a51f5bf24
YZY00Raiser <2452658175@qq.com>
2026-07-07 16:40:36 +0800

    update

---
+++ test/registered/ascend/basic_function/1/eplb/test_npu_ep_dispatch_algorithm.py
@@ -0,0 +1,104 @@
+import glob
+import os
+import unittest
+
+import requests
+
+from sglang.srt.utils import kill_process_tree
+# from sglang.test.ascend.test_ascend_utils import (
+#     QWEN3_30B_A3B_INSTRUCT_2507_WEIGHTS_PATH,
+# )
+from sglang.test.ci.ci_register import register_npu_ci
+from sglang.test.test_utils import (
+    DEFAULT_TIMEOUT_FOR_SERVER_LAUNCH,
+    DEFAULT_URL_FOR_TEST,
+    CustomTestCase,
+    popen_launch_server,
+)
+
+register_npu_ci(est_time=400, suite="full-2-npu-a3", nightly=True)
+QWEN3_30B_A3B_INSTRUCT_2507_WEIGHTS_PATH="/home/weights/Qwen/Qwen3-30B-A3B-Instruct-2507"
+
+class TestExpertDistributionRecorderModeStatic(CustomTestCase):
+    """Testcase: Verify set the parameter --expert-distribution-recorder-mode，
+    will generate .pt file and the inference request successfully.
+
+    [Test Category] Parameter
+    [Test Target] --expert-distribution-recorder-mode
+    """
+
+    ep_dispatch_algorithm = "dynamic"
+
+
+    @classmethod
+    def setUpClass(cls):
+        cls.process = popen_launch_server(
+            QWEN3_30B_A3B_INSTRUCT_2507_WEIGHTS_PATH,
+            DEFAULT_URL_FOR_TEST,
+            DEFAULT_TIMEOUT_FOR_SERVER_LAUNCH,
+            other_args=[
+                "--trust-remote-code",
+                "--attention-backend",
+                "ascend",
+                "--disable-cuda-graph",
+                "--mem-fraction-static",
+                "0.8",
+                "--tp-size",
+                "2",
+                "--expert-parallel-size",
+                "2",
+                "--enable-eplb",
+                "--moe-a2a-backend",
+                "deepep",
+                "--deepep-mode",
+                "normal",
+                "--ep-dispatch-algorithm",
+                cls.ep_dispatch_algorithm,
+            ],
+            env={
+                "HCCL_BUFFSIZE": "1024",
+                "TRANSFORMERS_VERBOSITY": "error",
+            },
+        )
+
+    @classmethod
+    def tearDownClass(cls):
+        kill_process_tree(cls.process.pid)
+
+    def test_recorder_mode(self):
+        # Start recording
+        requests.post(f"{DEFAULT_URL_FOR_TEST}/start_expert_distribution_record")
+
+        response = requests.post(
+            f"{DEFAULT_URL_FOR_TEST}/generate",
+            json={
+                "text": "The capital of France is",
+                "sampling_params": {
+                    "temperature": 0,
+                    "max_new_tokens": 32,
+                },
+            },
+        )
+        self.assertEqual(
+            response.status_code, 200, "The request status code is not 200."
+        )
+        self.assertIn(
+            "Paris", response.text, "The inference result does not include Paris."
+        )
+
+
+
+class TestExpertDistributionRecorderModeStatApprox(
+    TestExpertDistributionRecorderModeStatic
+):
+    ep_dispatch_algorithm = "fake"
+
+
+class TestExpertDistributionRecorderPerPass(CustomTestCase):
+    ep_dispatch_algorithm = "per_pass"
+
+
+
+
+if __name__ == "__main__":
+    unittest.main()
