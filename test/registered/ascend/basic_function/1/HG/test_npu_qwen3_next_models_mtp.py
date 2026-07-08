c9cd4942ea29bf7903279154d02efc4cfc776ad0
YZY00Raiser <2452658175@qq.com>
2026-07-08 22:36:34 +0800

    update

--- test/registered/ascend/basic_function/1/HG/test_npu_qwen3_next_models_mtp.py
+++ test/registered/ascend/basic_function/1/HG/test_npu_qwen3_next_models_mtp.py
@@ -4,7 +4,8 @@ from sglang.test.ci.ci_register import register_npu_ci
 from sglang.test.kits.eval_accuracy_kit import GSM8KMixin
 from sglang.test.kits.kl_divergence_kit import KLDivergenceMixin
 from sglang.test.kits.prefix_cache_branching_kit import PrefixCacheBranchingMixin
-from sglang.test.server_fixtures.default_fixture import DefaultServerBase
+from sglang.test.server_fixtures.default_fixture import DefaultServerBase, openai_api_env
+from sglang.test.test_utils import popen_launch_server
 
 register_npu_ci(est_time=600, suite="full-4-npu-a3", nightly=True)
 
@@ -40,6 +41,25 @@ class TestQwen3NextMTPV2(GSM8KMixin, KLDivergenceMixin, DefaultServerBase):
         "--attention-backend",
         "ascend",
     ]
+
+    @classmethod
+    def setUpClass(cls):
+        assert cls.model is not None, "Please set cls.model in subclass"
+
+        # Set OpenAI API key and base URL environment variables.
+        # Needed for lmm-evals to work.
+        with openai_api_env(cls.api_key):
+            cls.process = popen_launch_server(
+                cls.model,
+                cls.base_url,
+                timeout=cls.timeout,
+                other_args=cls.other_args,
+                env={
+                    "SGLANG_ENABLE_SPEC_V2": "1",
+                }
+            )
+
+
 
 
 if __name__ == "__main__":
