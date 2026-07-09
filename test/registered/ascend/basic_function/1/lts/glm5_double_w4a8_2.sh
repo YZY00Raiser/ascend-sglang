74db1fcb25bf2b662f902ca7d7edffe02c1bc813
YZY00Raiser <2452658175@qq.com>
2026-07-09 10:36:25 +0800

    update

--- test/registered/ascend/basic_function/1/lts/glm5_double_w4a8_2.sh
+++ test/registered/ascend/basic_function/1/lts/glm5_double_w4a8_2.sh
@@ -1,8 +1,9 @@
-export PYTHONPATH=/sgl-workspace/sglang/python:$PYTHONPATH
+LOG_DIR=/data/y/glm5_2_double_w8a8_logs
+LOG_FILE="$LOG_DIR/bench_serving.log"
+
 while true; do
-    LOG_FILE="$LOG_DIR/bench_serving_$(date +%Y%m%d_%H%M%S).log"
-    echo "=== Starting bench_serving at $(date) ==="
-    echo "=== Bench log: $LOG_FILE ==="
+    echo "=== Starting bench_serving at $(date) ===" >> "$LOG_FILE"
+    echo "=== Bench log: $LOG_FILE ===" >> "$LOG_FILE"
     python -m sglang.bench_serving \
     --dataset-name random --backend sglang \
     --model /root/.cache/modelscope/hub/models/Eco-Tech/GLM-5.2-w8a8 \
@@ -10,5 +11,5 @@ while true; do
     --host 172.22.3.71 --port 6688 --max-concurrency 128 \
     --random-input-len 3500 --random-output-len 1500 \
     --num-prompts 128 --random-range-ratio 1 \
-    > "$LOG_FILE" 2>&1
+    >> "$LOG_FILE" 2>&1
 done
