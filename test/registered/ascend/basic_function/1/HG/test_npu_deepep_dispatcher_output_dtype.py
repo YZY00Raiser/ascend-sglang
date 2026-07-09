7ad5821c35fd4209d0fa72464990843dacf2648a
YZY00Raiser <2452658175@qq.com>
2026-07-09 08:43:14 +0800

    update

--- test/registered/ascend/basic_function/1/HG/test_npu_deepep_dispatcher_output_dtype.py
+++ test/registered/ascend/basic_function/1/HG/test_npu_deepep_dispatcher_output_dtype.py
@@ -69,7 +69,7 @@ class TestDtypeAuto(CustomTestCase):
         metrics = run_eval(args)
         print(f"Eval accuracy of GSM8K: {metrics=}")
 
-        self.assertGreater(metrics["score"], 0.83)
+        self.assertGreater(metrics["score"], 0.74)
 
 class TestDtypeBf16(TestDtypeAuto):
     dtype = "bf16"
