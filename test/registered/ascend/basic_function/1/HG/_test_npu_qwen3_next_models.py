31d549b3da020b5ce42a9c4bb80545b4af7671d9
YZY00Raiser <2452658175@qq.com>
2026-07-13 09:53:00 +0800

    update

---
+++ test/registered/ascend/basic_function/1/HG/_test_npu_qwen3_next_models.py
@@ -0,0 +1,46 @@
+import unittest
+
+from sglang.test.ci.ci_register import register_npu_ci
+
+from sglang.test.kits.eval_accuracy_kit import GSM8KMixin
+from sglang.test.kits.kl_divergence_kit import KLDivergenceMixin
+from sglang.test.kits.prefix_cache_branching_kit import PrefixCacheBranchingMixin
+from sglang.test.server_fixtures.default_fixture import DefaultServerBase
+
+from sglang.test.ascend.test_ascend_utils import (
+    QWEN3_NEXT_80B_A3B_INSTRUCT_WEIGHTS_PATH,
+)
+
+register_npu_ci(est_time=600, suite="full-4-npu-a3", nightly=True)
+
+
+class TestQwen3Next(
+    GSM8KMixin, KLDivergenceMixin, PrefixCacheBranchingMixin, DefaultServerBase
+):
+    model = QWEN3_NEXT_80B_A3B_INSTRUCT_WEIGHTS_PATH
+    cache_chunk_size = 64
+    gsm8k_accuracy_thres = 0.93
+    #kl_div_thres = 0.0025
+    kl_div_thres = 0.02
+    other_args = [
+        "--tp-size",
+        "4",
+        "--chunked-prefill-size",
+        "1024",
+        "--mamba-scheduler-strategy",
+        "extra_buffer",
+        "--mamba-ssm-dtype",
+        "bfloat16",
+        "--mamba-track-interval",
+        "128",
+        "--page-size",
+        "128",
+        "--mem-fraction-static",
+        0.8,
+        "--attention-backend",
+        "ascend",
+    ]
+
+
+if __name__ == "__main__":
+    unittest.main()
