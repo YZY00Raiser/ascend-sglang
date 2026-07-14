4572e571ff451b5b47332d8e4e67d8ff4dd1b39a
YZY00Raiser <2452658175@qq.com>
2026-07-14 20:36:24 +0800

    update

--- test/registered/ascend/basic_function/1/714/npu/test_npu_lora_overlap_loading.py
+++ test/registered/ascend/basic_function/1/714/npu/test_npu_lora_overlap_loading.py
@@ -2,7 +2,7 @@ import multiprocessing as mp
 import unittest
 from sglang.test.ci.ci_register import register_npu_ci
 from sglang.test.lora_utils import (
-    # CI_MULTI_LORA_MODELS,
+    CI_MULTI_LORA_MODELS,
     run_lora_batch_splitting_equivalence_test, LoRAModelCase, LoRAAdaptor,
 )
 from sglang.test.test_utils import CustomTestCase
@@ -20,8 +20,8 @@ class TestLoRAOverlapLoading(CustomTestCase):
                 adaptors=[
                     LoRAAdaptor(
                         name="/home/weights/codelion/Llama-3.2-1B-Instruct-tool-calling-lora",
-                        # prefill_tolerance=1e-1,
-                        # rouge_l_tolerance=0.9,
+                        prefill_tolerance=1e-1,
+                        rouge_l_tolerance=0.9,
                     ),
                     LoRAAdaptor(
                         name="/home/weights/codelion/FastLlama-3.2-LoRA",
