8bb0447a0e722e57f565deda8e9c7e9d149e87c8
YZY00Raiser <2452658175@qq.com>
2026-07-14 19:02:14 +0800

    update

--- test/registered/ascend/basic_function/1/714/npu/test_npu_lora_overlap_loading.py
+++ test/registered/ascend/basic_function/1/714/npu/test_npu_lora_overlap_loading.py
@@ -20,13 +20,13 @@ class TestLoRAOverlapLoading(CustomTestCase):
                 adaptors=[
                     LoRAAdaptor(
                         name="/home/weights/codelion/Llama-3.2-1B-Instruct-tool-calling-lora",
-                        prefill_tolerance=1e-1,
-                        rouge_l_tolerance=0.9,
+                        # prefill_tolerance=1e-1,
+                        # rouge_l_tolerance=0.9,
                     ),
                     LoRAAdaptor(
                         name="/home/weights/codelion/FastLlama-3.2-LoRA",
-                        prefill_tolerance=3e-1,
-                        rouge_l_tolerance=0.9,
+                        # prefill_tolerance=3e-1,
+                        # rouge_l_tolerance=0.9,
                     ),
                 ],
                 max_loras_per_batch=2,
