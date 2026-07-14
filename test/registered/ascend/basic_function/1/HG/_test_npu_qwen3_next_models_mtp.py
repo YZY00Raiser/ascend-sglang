9038dbd61d413ecf0a30f6d4c5e18a858ea2bd87
YZY00Raiser <2452658175@qq.com>
2026-07-14 11:27:05 +0800

    update

--- test/registered/ascend/basic_function/1/HG/_test_npu_qwen3_next_models_mtp.py
+++ test/registered/ascend/basic_function/1/HG/_test_npu_qwen3_next_models_mtp.py
@@ -1,14 +1,16 @@
 import unittest
 
-from sglang.test.ci.ci_register import register_cuda_ci
+from sglang.test.ci.ci_register import register_npu_ci
 from sglang.test.kits.eval_accuracy_kit import GSM8KMixin
 from sglang.test.kits.kl_divergence_kit import KLDivergenceMixin
 from sglang.test.kits.prefix_cache_branching_kit import PrefixCacheBranchingMixin
-from sglang.test.server_fixtures.default_fixture import DefaultServerBase
+from sglang.test.server_fixtures.default_fixture import DefaultServerBase, openai_api_env
+from sglang.test.test_utils import popen_launch_server
+
 # from sglang.test.ascend.test_ascend_utils import (
 #     QWEN3_NEXT_80B_A3B_INSTRUCT_WEIGHTS_PATH,
 # )
-register_cuda_ci(est_time=290, stage="base-c", runner_config="4-gpu-h100")
+register_npu_ci(est_time=600, suite="full-4-npu-a3", nightly=True)
 
 QWEN3_NEXT_80B_A3B_INSTRUCT_WEIGHTS_PATH="/home/weights/Qwen3-Next-80B-A3B-Instruct"
 
@@ -75,6 +77,23 @@ class TestQwen3NextMTPV2(GSM8KMixin, KLDivergenceMixin, DefaultServerBase):
         "--attention-backend",
         "ascend",
     ]
+
+    @classmethod
+    def setUpClass(cls):
+        assert cls.model is not None, "Please set cls.model in subclass"
+
+        # Set OpenAI API key and base URL environment variables.
+        # Needed for lmm-evals to work.
+        with openai_api_env(cls.api_key):
+            cls.process = popen_launch_server(
+                cls.model,
+                cls.base_url,
+                timeout=cls.timeout,
+                other_args=cls.other_args,
+                env={
+                    "SGLANG_ENABLE_SPEC_V2": "1",
+                }
+            )
 
 
 if __name__ == "__main__":
