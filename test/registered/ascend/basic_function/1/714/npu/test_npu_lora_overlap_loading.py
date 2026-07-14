f86582756a73a093159ead2b17a44dd2e6c09e92
YZY00Raiser <2452658175@qq.com>
2026-07-14 19:00:08 +0800

    update

--- test/registered/ascend/basic_function/1/714/npu/test_npu_lora_overlap_loading.py
+++ test/registered/ascend/basic_function/1/714/npu/test_npu_lora_overlap_loading.py
@@ -2,7 +2,7 @@ import multiprocessing as mp
 import unittest
 from sglang.test.ci.ci_register import register_npu_ci
 from sglang.test.lora_utils import (
-    CI_MULTI_LORA_MODELS,
+    # CI_MULTI_LORA_MODELS,
     run_lora_batch_splitting_equivalence_test, LoRAModelCase, LoRAAdaptor,
 )
 from sglang.test.test_utils import CustomTestCase
@@ -11,27 +11,28 @@ register_npu_ci(est_time=600, suite="full-1-npu-a3", nightly=True)
 
 
 class TestLoRAOverlapLoading(CustomTestCase):
-    CI_MULTI_LORA_MODELS = [
-        # multi-rank case
-        LoRAModelCase(
-            base="/home/weights/Llama-3.2-1B-Instruct",
-            adaptors=[
-                LoRAAdaptor(
-                    name="/home/weights/codelion/Llama-3.2-1B-Instruct-tool-calling-lora",
-                    prefill_tolerance=1e-1,
-                    rouge_l_tolerance=0.9,
-                ),
-                LoRAAdaptor(
-                    name="/home/weights/codelion/FastLlama-3.2-LoRA",
-                    prefill_tolerance=3e-1,
-                    rouge_l_tolerance=0.9,
-                ),
-            ],
-            max_loras_per_batch=2,
-            max_loaded_loras=4,
-        ),
-    ]
+
     def test_ci_lora_models_batch_splitting(self):
+        CI_MULTI_LORA_MODELS = [
+            # multi-rank case
+            LoRAModelCase(
+                base="/home/weights/Llama-3.2-1B-Instruct",
+                adaptors=[
+                    LoRAAdaptor(
+                        name="/home/weights/codelion/Llama-3.2-1B-Instruct-tool-calling-lora",
+                        prefill_tolerance=1e-1,
+                        rouge_l_tolerance=0.9,
+                    ),
+                    LoRAAdaptor(
+                        name="/home/weights/codelion/FastLlama-3.2-LoRA",
+                        prefill_tolerance=3e-1,
+                        rouge_l_tolerance=0.9,
+                    ),
+                ],
+                max_loras_per_batch=2,
+                max_loaded_loras=4,
+            ),
+        ]
         run_lora_batch_splitting_equivalence_test(
             CI_MULTI_LORA_MODELS,
             enable_lora_overlap_loading=True,
