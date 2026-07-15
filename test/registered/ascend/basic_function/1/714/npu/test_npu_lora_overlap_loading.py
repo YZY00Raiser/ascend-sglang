223c1934afea60ace78fdbe918257245cb5a4146
YZY00Raiser <2452658175@qq.com>
2026-07-15 20:31:34 +0800

    update

--- test/registered/ascend/basic_function/1/714/npu/test_npu_lora_overlap_loading.py
+++ test/registered/ascend/basic_function/1/714/npu/test_npu_lora_overlap_loading.py
@@ -16,7 +16,7 @@ class TestLoRAOverlapLoading(CustomTestCase):
         CI_MULTI_LORA_MODELS = [
             # multi-rank case
             LoRAModelCase(
-                base="/home/weights/Qwen3.5-4BB",
+                base="/home/weights/Qwen3.5-4B",
                 adaptors=[
                     LoRAAdaptor(
                         name="/home/weights/qwen3.5-4b-mcat-lor",
