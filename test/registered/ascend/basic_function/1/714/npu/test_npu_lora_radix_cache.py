bc14339cd93d41dbd61cf4618950bf27bd1aa3c2
YZY00Raiser <2452658175@qq.com>
2026-07-15 16:12:03 +0800

    update

--- test/registered/ascend/basic_function/1/714/npu/test_npu_lora_radix_cache.py
+++ test/registered/ascend/basic_function/1/714/npu/test_npu_lora_radix_cache.py
@@ -50,6 +50,7 @@ class TestLoRARadixCache(CustomTestCase):
 
         model_case = CI_MULTI_LORA_MODELS[0]
 
+        print("model_case.skip_long_prompt")
         print(model_case.skip_long_prompt)
 
         torch_dtype = torch.float16
