31cc1e5ad066a3327bd6e6fc5f91657d67b14332
YZY00Raiser <2452658175@qq.com>
2026-07-15 16:29:18 +0800

    update

---
+++ test/registered/ascend/basic_function/1/714/npu/test_npu_lora_qwen3.py
@@ -0,0 +1,54 @@
+# Copyright 2023-2025 SGLang Team
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+
+import multiprocessing as mp
+import unittest
+
+from sglang.test.lora_utils import (
+    # LORA_MODELS_QWEN3,
+    run_lora_multiple_batch_on_model_cases, LoRAModelCase, LoRAAdaptor,
+)
+from sglang.test.ci.ci_register import register_npu_ci
+from sglang.test.test_utils import CustomTestCase
+
+register_npu_ci(est_time=100, suite="full-1-npu-a3", nightly=True)
+LORA_MODELS_QWEN3 = [
+    LoRAModelCase(
+        base="/home/weights/Qwen3-4B",
+        adaptors=[
+            LoRAAdaptor(
+                name="/home/weights/Qwen3-4B-lora-v2",
+                prefill_tolerance=3e-1,
+            ),
+            LoRAAdaptor(
+                name="/home/weights/Qwen3-4B-LoRA-ZH-WebNovelty-v0.0",
+                prefill_tolerance=3e-1,
+            ),
+        ],
+        max_loras_per_batch=2,
+    ),
+]
+
+class TestLoRAQwen3(CustomTestCase):
+    def test_ci_lora_models(self):
+        run_lora_multiple_batch_on_model_cases(LORA_MODELS_QWEN3)
+
+
+if __name__ == "__main__":
+    try:
+        mp.set_start_method("spawn")
+    except RuntimeError:
+        pass
+
+    unittest.main(warnings="ignore")
