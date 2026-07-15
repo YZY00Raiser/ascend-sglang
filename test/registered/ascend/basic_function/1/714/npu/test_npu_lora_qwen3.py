be77f855cab3725800807bbc56b6e1634769b3e9
YZY00Raiser <2452658175@qq.com>
2026-07-15 16:55:21 +0800

    update

--- test/registered/ascend/basic_function/1/714/npu/test_npu_lora_qwen3.py
+++ test/registered/ascend/basic_function/1/714/npu/test_npu_lora_qwen3.py
@@ -42,7 +42,9 @@ LORA_MODELS_QWEN3 = [
 
 class TestLoRAQwen3(CustomTestCase):
     def test_ci_lora_models(self):
-        run_lora_multiple_batch_on_model_cases(LORA_MODELS_QWEN3)
+        run_lora_multiple_batch_on_model_cases(
+            LORA_MODELS_QWEN3,
+            attention_backend="ascend",)
 
 
 if __name__ == "__main__":
