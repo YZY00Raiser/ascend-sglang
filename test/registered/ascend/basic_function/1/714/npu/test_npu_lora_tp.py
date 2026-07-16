94a6c95ea0723fe470a7d1ab77689b1324a47d6d
YZY00Raiser <2452658175@qq.com>
2026-07-16 16:32:38 +0800

    update

--- test/registered/ascend/basic_function/1/714/npu/test_npu_lora_tp.py
+++ test/registered/ascend/basic_function/1/714/npu/test_npu_lora_tp.py
@@ -1,23 +1,9 @@
-# Copyright 2023-2024 SGLang Team
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-# ==============================================================================
-
 import multiprocessing as mp
 import os
 import unittest
 from typing import List, Optional
 
-from sglang.test.ci.ci_register import register_npu_ci
+from sglang.test.ci.ci_register import register_amd_ci, register_cuda_ci
 from sglang.test.lora_utils import (
     ALL_OTHER_LORA_MODELS,
     CI_LORA_MODELS,
@@ -27,8 +13,13 @@ from sglang.test.lora_utils import (
     LoRAModelCase,
     run_lora_test_one_by_one,
 )
-register_npu_ci(est_time=600, suite="full-1-npu-a3", nightly=True)
+from sglang.test.test_utils import CustomTestCase, is_in_ci
 
+register_cuda_ci(
+    est_time=230,
+    stage="base-c",
+    runner_config="8-gpu-h200",
+)
 register_amd_ci(
     est_time=116,
     suite="stage-b-test-2-gpu-large-amd",
