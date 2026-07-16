9524cc22f95fcb50d52dcf97bb71f208de530efd
YZY00Raiser <2452658175@qq.com>
2026-07-16 09:57:03 +0800

    update

--- test/registered/ascend/basic_function/1/714/npu/test_npu_deepep_large.py
+++ test/registered/ascend/basic_function/1/714/npu/test_npu_deepep_large.py
@@ -19,7 +19,7 @@ register_npu_ci(est_time=200, suite="full-16-npu-a3", nightly=True)
 DEEPSEEK_V32_MODEL_PATH = "/home/weights/DeepSeek-V2-Lite-W8A8"
 
 
-@unittest.skip("Skip for saving ci time")
+# @unittest.skip("Skip for saving ci time")
 class TestDeepseek(CustomTestCase):
     @classmethod
     def setUpClass(cls):
