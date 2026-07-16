cb75179f9ea0910bf9e54f9fc34c252ad0e691e8
YZY00Raiser <2452658175@qq.com>
2026-07-16 14:24:00 +0800

    update

--- test/registered/ascend/basic_function/1/714/npu/test_npu_lora_radix_cache.py
+++ test/registered/ascend/basic_function/1/714/npu/test_npu_lora_radix_cache.py
@@ -33,7 +33,7 @@ class TestLoRARadixCache(CustomTestCase):
                 adaptors=[
                     LoRAAdaptor(
                         name="/home/weights/Qwen3-4B-lora-v2",
-                        prefill_tolerance=1,
+                        prefill_tolerance=5,
                         rouge_l_tolerance=0.9,
                     ),
                     LoRAAdaptor(
