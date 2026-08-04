7860f03c65dbc3486c4409ab829f9d1203a9289f
YZY00Raiser <2452658175@qq.com>
2026-08-04 17:26:24 +0800

    update

--- test/registered/ascend/basic_function/1/714/npu/test_npu_deepep_large.py
+++ test/registered/ascend/basic_function/1/714/npu/test_npu_deepep_large.py
@@ -132,7 +132,7 @@ class TestDeepseekMTP(CustomTestCase):
                 "--model-loader-extra-config",
                 '{"enable_multithread_load": true,"num_threads": 64}',
                 "--mem-fraction-static",
-                "0.67",
+                "0.55",
                 "--attention-backend",
                 "ascend",
                 "--quantization",
