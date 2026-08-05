dce6db87d788b952be4e266e360309538ea17de8
YZY00Raiser <2452658175@qq.com>
2026-08-05 19:35:59 +0800

    update

--- test/registered/ascend/basic_function/1/714/npu/test_npu_lora_tp.py
+++ test/registered/ascend/basic_function/1/714/npu/test_npu_lora_tp.py
@@ -22,11 +22,13 @@ register_npu_ci(est_time=500, suite="full-2-npu-a3", nightly=True)
 ALL_OTHER_LORA_MODELS = [
     LoRAModelCase(
         # base="meta-llama/Llama-3.1-8B-Instruct",
-        base="/home/weights/Qwen3.5-9B",
+        # base="/home/weights/Qwen3.5-9B",
+        base="/home/weights/Qwen/Qwen3.6-27B-W8A8",
         adaptors=[
             LoRAAdaptor(
                 # name="nvidia/llama-3.1-nemoguard-8b-topic-control",
-                name="/home/weights/Qwen3.5-9B-LoRA-new",
+                # name="/home/weights/Qwen3.5-9B-LoRA-new",
+                name="qwen3.6-27b-cybersecurity-lora",
                 prefill_tolerance=7e-1,
                 decode_tolerance=7,
             ),
