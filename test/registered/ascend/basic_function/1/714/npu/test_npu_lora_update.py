68b932d79eb0d662a7eb8debf9a2b94edee6bda2
YZY00Raiser <2452658175@qq.com>
2026-08-06 22:32:15 +0800

    update

--- test/registered/ascend/basic_function/1/714/npu/test_npu_lora_update.py
+++ test/registered/ascend/basic_function/1/714/npu/test_npu_lora_update.py
@@ -1510,7 +1510,8 @@ class TestLoRADynamicUpdate(CustomTestCase):
         """
         Test dynamic LoRA updates in server mode.
         """
-        test_cases = BASIC_TESTS #if is_in_ci() else ALL_TESTS
+        # test_cases = BASIC_TESTS #if is_in_ci() else ALL_TESTS
+        test_cases = TARGET_MODULE_TESTS
         self._run_dynamic_adapter_updates(
             mode=LoRAUpdateTestSessionMode.SERVER, test_cases=test_cases
         )
