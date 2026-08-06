60b75a545194a57c5591210c1102e3eba091056f
YZY00Raiser <2452658175@qq.com>
2026-08-06 11:08:12 +0800

    update

---
+++ test/registered/ascend/basic_function/1/714/npu/_test_npu_deepep_auto_deepseek_v2.py
@@ -0,0 +1,150 @@
+import unittest
+from types import SimpleNamespace
+
+import requests
+
+from sglang.srt.utils import kill_process_tree
+from sglang.test.ci.ci_register import register_npu_ci
+from sglang.test.run_eval import run_eval
+from sglang.test.test_utils import (
+    DEFAULT_TIMEOUT_FOR_SERVER_LAUNCH,
+    DEFAULT_URL_FOR_TEST,
+    CustomTestCase,
+    popen_launch_server,
+)
+
+register_npu_ci(est_time=1000, suite="full-2-npu-a3", nightly=True)
+
+DEEPSEEK_V32_MODEL_PATH = "/home/weights/DeepSeek-V2-Lite-W8A8"
+
+
+class TestDeepseekMTP(CustomTestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.model = DEEPSEEK_V32_MODEL_PATH
+        cls.base_url = DEFAULT_URL_FOR_TEST
+        cls.process = popen_launch_server(
+            cls.model,
+            cls.base_url,
+            timeout=DEFAULT_TIMEOUT_FOR_SERVER_LAUNCH,
+            other_args=[
+                "--disable-overlap-schedule",
+                "--trust-remote-code",
+                "--tp",
+                "2",
+                "--enable-dp-attention",
+                "--dp",
+                "2",
+                "--moe-dense-tp-size",
+                "1",
+                "--enable-dp-lm-head",
+                "--moe-a2a-backend",
+                "deepep",
+                "--ep-num-redundant-experts",
+                "32",
+                "--ep-dispatch-algorithm",
+                "dynamic",
+                "--eplb-algorithm",
+                "deepseek",
+                "--cuda-graph-bs",
+                "64",
+                "--max-running-requests",
+                "512",
+                "--speculative-algorithm",
+                # "NEXTN",
+                # "EAGLE3",
+                "EAGLE",
+                "--speculative-num-steps",
+                "1",
+                "--speculative-eagle-topk",
+                "1",
+                "--speculative-num-draft-tokens",
+                "2",
+                "--disable-radix-cache",
+                "--model-loader-extra-config",
+                '{"enable_multithread_load": true,"num_threads": 64}',
+                "--mem-fraction-static",
+                "0.55",
+                "--attention-backend",
+                "ascend",
+                # "--quantization",
+                # "modelslim",
+                "--trust-remote-code",
+                "--attention-backend",
+                "ascend",
+                "--tp-size",
+                "8",
+                "--moe-a2a-backend",
+                "deepep",
+                "--deepep-mode",
+                "auto",
+                "--disable-cuda-graph",
+                "--dp-size",
+                "8",
+                "--enable-dp-attention",
+                "--chunked-prefill-size",
+                "1024",
+                "--mem-fraction-static",
+                "0.7",
+
+            ],
+            env={
+                "SGLANG_ENABLE_JIT_DEEPGEMM": "0",
+                "HCCL_BUFFSIZE": "2048",
+                "MOE_ENABLE_TOPK_NEG_ONE": "1",
+                "DEEP_NORMAL_MODE_USE_INT8_QUANT": "1",
+                # "DEEPEP_HCCL_BUFFSIZE": "1800",
+                "SGLANG_ENABLE_SPEC_V2": "1",
+                "SGLANG_DEEPEP_NUM_MAX_DISPATCH_TOKENS_PER_RANK": "512",
+            },
+        )
+
+    @classmethod
+    def tearDownClass(cls):
+        kill_process_tree(cls.process.pid)
+
+    def test_mmlu(self):
+        expect_score = 0.58
+        args = SimpleNamespace(
+            base_url=self.base_url,
+            model=self.model,
+            eval_name="mmlu",
+            num_examples=128,
+            num_threads=32,
+            api="completion",
+            num_shots=5,
+        )
+        metrics = run_eval(args)
+        self.assertGreater(metrics["score"], expect_score)
+
+    def test_gsm8k(self):
+        args = SimpleNamespace(
+            base_url=self.base_url,
+            model=self.model,
+            eval_name="gsm8k",
+            api="completion",
+            max_tokens=512,
+            num_examples=1200,
+            num_threads=1200,
+        )
+        metrics = run_eval(args)
+        print(f"Eval accuracy of GSM8K: {metrics=}")
+
+        self.assertGreater(metrics["score"], 0.34)
+
+        server_info = requests.get(self.base_url + "/server_info")
+        print("WWWwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww")
+        print(server_info.json())
+        avg_spec_accept_length = server_info.json()["internal_states"][0][
+            "avg_spec_accept_length"
+        ]
+        print(
+            f"###test_gsm8k:\n"
+            f"accuracy={metrics['score']=:.3f}\n"
+            f"{avg_spec_accept_length=:.3f}\n"
+        )
+        self.assertGreater(avg_spec_accept_length, 1.85)
+
+
+if __name__ == "__main__":
+    unittest.main()
