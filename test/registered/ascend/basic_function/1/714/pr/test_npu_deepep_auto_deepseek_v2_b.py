163c7efdc1b8ba99c225ede85cc0ade4e17bc555
YZY00Raiser <2452658175@qq.com>
2026-08-24 20:57:27 +0800

    update

--- test/registered/ascend/basic_function/1/714/pr/test_npu_deepep_auto_deepseek_v2_b.py
+++ test/registered/ascend/basic_function/1/714/pr/test_npu_deepep_auto_deepseek_v2_b.py
@@ -7,13 +7,13 @@ import requests
 from sglang.test.ascend.gsm8k_ascend_mixin import GSM8KAscendMixin
 from sglang.test.ascend.test_mmlu import TestMMLU
 from sglang.test.ascend.run_eval import run_eval
-from sglang.test.ascend.test_ascend_utils import DEEPSEEK_V2_LITE_W8A8_WEIGHTS_PATH
+# from sglang.test.ascend.test_ascend_utils import DEEPSEEK_V2_LITE_W8A8_WEIGHTS_PATH
 from sglang.test.ci.ci_register import register_npu_ci
 from sglang.test.test_utils import CustomTestCase
 
 register_npu_ci(est_time=400, suite="full-8-npu-a3", nightly=True)
 
-
+DEEPSEEK_V2_LITE_W8A8_WEIGHTS_PATH="/home/weights/DeepSeek-V2-Lite-W8A8"
 class TestDeepEpDeepseek(GSM8KAscendMixin, TestMMLU, CustomTestCase):
     model = DEEPSEEK_V2_LITE_W8A8_WEIGHTS_PATH
     other_args = [
