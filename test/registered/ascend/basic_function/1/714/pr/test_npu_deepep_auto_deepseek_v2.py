105b290d23924124dc3e2676faac0e6af13e3963
YZY00Raiser <2452658175@qq.com>
2026-08-25 19:47:54 +0800

    update

--- test/registered/ascend/basic_function/1/714/pr/test_npu_deepep_auto_deepseek_v2.py
+++ test/registered/ascend/basic_function/1/714/pr/test_npu_deepep_auto_deepseek_v2.py
@@ -1,9 +1,8 @@
 import os
 import unittest
-from types import SimpleNamespace
 
 from sglang.test.ascend.gsm8k_ascend_mixin import GSM8KAscendMixin
-from sglang.test.ascend.run_eval import run_eval as run_ascend_eval
+from sglang.test.ascend.test_mmlu import TestMMLU
 from sglang.test.ascend.test_ascend_utils import DEEPSEEK_V2_LITE_W8A8_WEIGHTS_PATH
 from sglang.test.ci.ci_register import register_npu_ci
 from sglang.test.test_utils import CustomTestCase
@@ -11,9 +10,8 @@ from sglang.test.test_utils import CustomTestCase
 register_npu_ci(est_time=400, suite="full-8-npu-a3", nightly=True)
 
 
-class TestDeepEpDeepseek(GSM8KAscendMixin, CustomTestCase):
+class TestDeepEpDeepseek(GSM8KAscendMixin, CustomTestCase, TestMMLU):
     model = DEEPSEEK_V2_LITE_W8A8_WEIGHTS_PATH
-    accuracy = 0.34
     other_args = [
         "--trust-remote-code",
         "--attention-backend",
@@ -42,19 +40,9 @@ class TestDeepEpDeepseek(GSM8KAscendMixin, CustomTestCase):
         "DEEP_NORMAL_MODE_USE_INT8_QUANT": "1",
     }
 
-    def test_mmlu(self):
-        expect_score = 0.58
-        args = SimpleNamespace(
-            base_url=self.base_url,
-            model=self.model,
-            eval_name="mmlu",
-            num_examples=128,
-            num_threads=32,
-            api="completion",
-            num_shots=5,
-        )
-        metrics = run_ascend_eval(args)
-        self.assertGreater(metrics["score"], expect_score)
+    accuracy = 0.34  # Test GSM8K accuracy ≥0.34
+    accuracy_mmlu = 0.38  # Test MMLU accuracy ≥0.38
+
 
 
 if __name__ == "__main__":
