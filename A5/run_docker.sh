c35a6c58efb0e80726a14ea7f620d741bb222d41
YZY00Raiser <2452658175@qq.com>
2026-08-25 11:28:05 +0800

    update

--- A5/run_docker.sh
+++ A5/run_docker.sh
@@ -38,4 +38,4 @@ docker run --name=yzy_deepep_test \
 	--runtime=runc \
 	--detach=true \
 	-t \
-	swr.cn‑south‑1.myhuaweicloud.com/mindie‑pymotor/mindie‑pymotor:3.1.0‑vllm_ascend0.23.0‑a5‑ubuntu22.04‑py3.12
+	vllm-ascend:dev-26.1.0.day20260627-A5-server-py311-openEuler24.03-lts-aarch64
