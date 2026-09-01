a1e236bd325ec4d62b281614e0dad8ebd0d6f942
YZY00Raiser <2452658175@qq.com>
2026-09-01 19:48:16 +0800

    update

--- test/registered/ascend/basic_function/1/831/test_npu_qwen3_30b_attn_cp.py
+++ test/registered/ascend/basic_function/1/831/test_npu_qwen3_30b_attn_cp.py
@@ -1,10 +1,15 @@
 import os
+import tempfile
 import unittest
+from types import SimpleNamespace
 
+from sglang.srt.utils import kill_process_tree
 from sglang.test.ascend.gsm8k_ascend_mixin import GSM8KAscendMixin
 from sglang.test.ascend.test_ascend_utils import QWEN3_30B_A3B_WEIGHTS_PATH
 from sglang.test.ci.ci_register import register_npu_ci
-from sglang.test.test_utils import CustomTestCase
+from sglang.test.run_eval import run_eval
+from sglang.test.test_utils import CustomTestCase, popen_launch_server, DEFAULT_URL_FOR_TEST, \
+    DEFAULT_TIMEOUT_FOR_SERVER_LAUNCH
 
 register_npu_ci(est_time=500, suite="full-4-npu-a3", nightly=True)
 
@@ -22,33 +27,65 @@ class TestQwen330BAttnCP(GSM8KAscendMixin, CustomTestCase):
     environment variables from the PD GSM8K test.
     """
 
-    model = QWEN3_30B_A3B_WEIGHTS_PATH
-    other_args = [
-        "--trust-remote-code",
-        "--mem-fraction-static",
-        "0.7",
-        "--max-running-requests",
-        "32",
-        "--attention-backend",
-        "ascend",
-        "--tp-size",
-        "4",
-        "--moe-dp-size",
-        "2",
-        "--attn-cp-size",
-        "2",
-        "--cuda-graph-max-bs-decode",
-        "32",
-        "--enable-prefill-context-parallel",
-    ]
+    @classmethod
+    def setUpClass(cls):
 
-    env = {**os.environ, "ASCEND_USE_FIA": "1"}
+        cls.model = QWEN3_30B_A3B_WEIGHTS_PATH
+        cls.base_url = DEFAULT_URL_FOR_TEST
+        cls.out_file = tempfile.NamedTemporaryFile(
+            mode="w+", suffix=".txt", delete=False
+        )
+        cls.err_file = tempfile.NamedTemporaryFile(
+            mode="w+", suffix=".txt", delete=False
+        )
+        cls.process = popen_launch_server(
+            cls.model,
+            cls.base_url,
+            timeout=DEFAULT_TIMEOUT_FOR_SERVER_LAUNCH,
+            other_args=[
+                "--trust-remote-code",
+                "--mem-fraction-static",
+                "0.7",
+                "--max-running-requests",
+                "32",
+                "--attention-backend",
+                "ascend",
+                "--tp-size",
+                "4",
+                "--moe-dp-size",
+                "2",
+                "--attn-cp-size",
+                "2",
+                "--cuda-graph-max-bs-decode",
+                "32",
+                "--enable-prefill-context-parallel",
+            ],
+            return_stdout_stderr=(cls.out_file, cls.err_file),
+            env = {**os.environ, "ASCEND_USE_FIA": "1"}
 
-    # GSM8K Configs
-    accuracy = 0.92  # GSM8K accuracy ≥0.92
-    gsm8k_parallel = 32
-    num_questions = 100
-    gsm8k_num_shots = 5
+        )
+
+    @classmethod
+    def tearDownClass(cls):
+        kill_process_tree(cls.process.pid)
+        cls.out_file.close()
+        cls.err_file.close()
+        os.unlink(cls.out_file.name)
+        os.unlink(cls.err_file.name)
+
+    def test_gsm8k(self):
+        args = SimpleNamespace(
+            base_url=self.base_url,
+            model=self.model,
+            eval_name="gsm8k",
+            api="completion",
+            max_tokens=512,
+            num_examples=100,
+            num_threads=32,
+        )
+        metrics = run_eval(args)
+        print(f"Eval accuracy of GSM8K: {metrics=}")
+        self.assertGreater(metrics["score"], 0.92)
 
 
 if __name__ == "__main__":
