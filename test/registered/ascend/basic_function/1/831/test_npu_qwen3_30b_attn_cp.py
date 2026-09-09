411c10921b67cf5f2cb015124492ae1125b6adb3
YZY00Raiser <2452658175@qq.com>
2026-09-09 14:06:41 +0800

    update

--- test/registered/ascend/basic_function/1/831/test_npu_qwen3_30b_attn_cp.py
+++ test/registered/ascend/basic_function/1/831/test_npu_qwen3_30b_attn_cp.py
@@ -87,7 +87,7 @@ class TestQwen330BAttnCP(GSM8KAscendMixin, CustomTestCase):
     def test_moe_dp(self):
         self.err_file.seek(0)
         content = self.err_file.read()
-        self.assertIn("MOE_DP2", content)
+        self.assertIn("MOE_DP1", content)
 
 
 if __name__ == "__main__":
