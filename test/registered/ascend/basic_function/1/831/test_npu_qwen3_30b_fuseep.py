7716137c0e9026469237fbce16fe8c54fb37970a
YZY00Raiser <2452658175@qq.com>
2026-09-10 20:21:44 +0800

    update

--- test/registered/ascend/basic_function/1/831/test_npu_qwen3_30b_fuseep.py
+++ test/registered/ascend/basic_function/1/831/test_npu_qwen3_30b_fuseep.py
@@ -6,23 +6,55 @@ from sglang.test.ascend.test_ascend_utils import QWEN3_30B_A3B_W8A8_WEIGHTS_PATH
 from sglang.test.ci.ci_register import register_npu_ci
 from sglang.test.test_utils import CustomTestCase
 
-register_npu_ci(est_time=400, suite="per-commit-4-npu-a3")
+register_npu_ci(est_time=400, suite="per-commit-8-npu-a3")
+
 
 QWEN3_30B_A3B_W8A8_WEIGHTS_PATH="/home/weights/Qwen3-30B-A3B-W8A8"
-class TestQwen330Bw8a8FuseModeWithTwo(GSM8KAscendMixin, CustomTestCase):
-    """Testcase: Verify that the inference accuracy of the Qwen/Qwen3-30B-A3B-w8a8 model on the GSM8K dataset is
-    no less than 0.90 with --fuseep-mode.
 
-    [Test Category] Model
-    [Test Target] Qwen/Qwen3-30B-A3B-w8a8
+# class TestQwen330Bw8a8FuseModeWithTwo(GSM8KAscendMixin, CustomTestCase):
+#     """Testcase: Verify that the inference accuracy of the Qwen/Qwen3-30B-A3B-w8a8 model on the GSM8K dataset is
+#     no less than 0.90 with --fuseep-mode.
+#
+#     [Test Category] Model
+#     [Test Target] Qwen/Qwen3-30B-A3B-w8a8
+#     """
+#
+#     model = QWEN3_30B_A3B_W8A8_WEIGHTS_PATH
+#     accuracy = 0.90
+#     """
+#     Use dispatch_ffn_combine operator, when fuseep_mode = 2, fusion of dispatch + GMM + combine only for the decode phase
+#     """
+#     fuseep_mode = 2
+#     other_args = [
+#         "--trust-remote-code",
+#         "--mem-fraction-static",
+#         0.8,
+#         "--attention-backend",
+#         "ascend",
+#         "--cuda-graph-max-bs-decode",
+#         128,
+#         "--tp-size",
+#         8,
+#         "--moe-a2a-backend",
+#         "ascend_fuseep",
+#         "--fuseep-mode",
+#         fuseep_mode,
+#     ]
+#
+#     env = {
+#         **os.environ,
+#         "SGLANG_PREFILL_DELAYER_MAX_DELAY_PASSES": "100",
+#         "SGLANG_DEEPEP_NUM_MAX_DISPATCH_TOKENS_PER_RANK": "188416",
+#     }
+
+
+class TestQwen330Bw8a8FuseModeWithOne(GSM8KAscendMixin, CustomTestCase):
     """
-
+    Use dispatch_gmm_combine_decode operator, when fuseep_mode = 1, Integrate dispatch, the entire FFN (including GMM),
+    and combine into one large operator.
+    """
     model = QWEN3_30B_A3B_W8A8_WEIGHTS_PATH
     accuracy = 0.90
-    """
-    Use dispatch_ffn_combine operator, when fuseep_mode = 2, fusion of dispatch + GMM + combine only for the decode phase
-    """
-    fuseep_mode = 2
     other_args = [
         "--trust-remote-code",
         "--mem-fraction-static",
@@ -33,28 +65,16 @@ class TestQwen330Bw8a8FuseModeWithTwo(GSM8KAscendMixin, CustomTestCase):
         128,
         "--tp-size",
         8,
+        "--ep-size",
+        "8",
         "--moe-a2a-backend",
         "ascend_fuseep",
         "--fuseep-mode",
-        fuseep_mode,
-    ]
-
-    env = {
-        **os.environ,
-        "SGLANG_PREFILL_DELAYER_MAX_DELAY_PASSES": "100",
-        "SGLANG_DEEPEP_NUM_MAX_DISPATCH_TOKENS_PER_RANK": "188416",
-    }
-
-
-class TestQwen330Bw8a8FuseModeWithOne(TestQwen330Bw8a8FuseModeWithTwo):
-    """
-    Use dispatch_gmm_combine_decode operator, when fuseep_mode = 1, Integrate dispatch, the entire FFN (including GMM),
-    and combine into one large operator.
-    """
-    fuseep_mode = 1
-    other_args = [
-        *TestQwen330Bw8a8FuseModeWithTwo.other_args[:-1],
-        fuseep_mode,
+        1,
+        "--max-prefill-tokens",
+        2048,
+        "--chunked-prefill-size",
+        2048,
     ]
 
     env = {
