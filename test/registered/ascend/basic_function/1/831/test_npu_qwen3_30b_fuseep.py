03fa532d714339eaa4f80b11d79a59dd9f390f15
YZY00Raiser <2452658175@qq.com>
2026-09-10 14:40:07 +0800

    update

--- test/registered/ascend/basic_function/1/831/test_npu_qwen3_30b_fuseep.py
+++ test/registered/ascend/basic_function/1/831/test_npu_qwen3_30b_fuseep.py
@@ -19,6 +19,9 @@ class TestQwen330Bw8a8FuseModeWithTwo(GSM8KAscendMixin, CustomTestCase):
 
     model = QWEN3_30B_A3B_W8A8_WEIGHTS_PATH
     accuracy = 0.90
+    """
+    Use dispatch_ffn_combine operator, when fuseep_mode = 2, fusion of dispatch + GMM + combine only for the decode phase
+    """
     fuseep_mode = 2
     other_args = [
         "--trust-remote-code",
@@ -42,8 +45,18 @@ class TestQwen330Bw8a8FuseModeWithTwo(GSM8KAscendMixin, CustomTestCase):
         "SGLANG_DEEPEP_NUM_MAX_DISPATCH_TOKENS_PER_RANK": "188416",
     }
 
-class TestQwen330Bw8a8FuseModeWithOne(TestQwen330Bw8a8FuseModeWithTwo):
-    fuseep_mode = 1
+
+# class TestQwen330Bw8a8FuseModeWithOne(TestQwen330Bw8a8FuseModeWithTwo):
+#     """
+#     Use dispatch_gmm_combine_decode operator, when fuseep_mode = 1, Integrate dispatch, the entire FFN (including GMM),
+#     and combine into one large operator.
+#     """
+#     fuseep_mode = 1
+#     other_args = [
+#         *TestQwen330Bw8a8FuseModeWithTwo.other_args[:-1],
+#         fuseep_mode,
+#     ]
+
 
 if __name__ == "__main__":
     unittest.main()
